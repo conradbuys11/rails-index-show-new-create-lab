067d01a65805c448320e3b64b4e9ac9d45e6246e
conrad_buys <conradbuys11@gmail.com>
2020-09-16 14:50:17 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/coupons', to: 'coupons#index'
-  get '/coupons/new', to: 'coupons#new'
+  get '/coupons/new', to: 'coupons#new', as: 'new_coupon'
   get '/coupons/:id', to: 'coupons#show', as: 'coupon'
   post '/coupons', to: 'coupons#create'
 
